1f995b96306cb5d561e5f5fed9e9dc0feac59632
root <root@localhost.localdomain>
2020-07-03 18:47:34 +0530

    shell scripting

---
+++ july32020/first.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo  "Hello World"
+echo  'hiii'
+echo  heyyyyyyyyyyyy
+
+#  very similar to printf 
+# defining  variables 
+x=100
+y=hello
+z=4.5
+echo $x
+echo $y
+echo $z
+#  how to take input from user 
---
+++ july32020/user_input.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo  "Plz  enter something :--   "
+sleep 2
+read  x
+#  here read is similar to scanf in c or input in python 
+
+echo "you have entere  $x"
+sleep 3
+## we can use another 
+read -p "Enter a number  "  y
+sleep 4
+echo $y
